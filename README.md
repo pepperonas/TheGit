16154fc429b018867c368293b6895e5c54fa2b32
martin <martinpaush@gmail.com>
2017-07-09 16:43:48 +0200

    initial commit
    Signed-off-by: martin <martinpaush@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# TheGit
+git stuff
---
+++ src/io/celox/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Changed: 2017.7.9 4:43:48
+ * User: martin
+ * File: Main.java
+ */
+
+package io.celox;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+    }
+}
+
